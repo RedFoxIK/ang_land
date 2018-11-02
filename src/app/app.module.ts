5fa6556139a8da557931988aaa2570a3aa179f9f
Yaroslava Kalashnyk <Yaroslava_Kalashnyk@epam.com>
2018-11-02 15:56:51 +0200

    add 2 new component: header and process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { HeaderComponent } from './header/header.component';
+import { ProcessComponent } from './process/process.component';
 
 const routes: Routes = [
   {
       path: '',
       pathMatch: 'full',
       component: ContactsComponent
-  }
+  },
+  {
+    path: 'process',
+    pathMatch: 'full',
+    component: ProcessComponent
+  },
 ];
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    ContactsComponent
+    ContactsComponent,
+    HeaderComponent,
+    ProcessComponent
   ],
   imports: [
     BrowserModule,
